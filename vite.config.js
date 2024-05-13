3894d99163a4eaf15fbc9791a189621476e96ec2
Houstin0 <houstin0000@gmail.com>
2024-05-13 21:28:44 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080, // Specify the desired port here
+    port: 4000, // Specify the desired port here
   },
 })
