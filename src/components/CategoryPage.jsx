7c2c956d7e4166bd673cffc3627a5fc7df3a9b8a
Houstin0 <houstin0000@gmail.com>
2024-07-04 01:46:06 +0300

    corrected margin

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -41,7 +41,7 @@ function CategoryPage({ addToCart, cartItems}) {
 
 
       <div>
-        <nav className="flex lg:ml-12" aria-label="Breadcrumb ">
+        <nav className="flex ml-4 lg:ml-12" aria-label="Breadcrumb ">
           <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
           <li className="inline-flex items-center">
       <a href="/" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
@@ -80,7 +80,7 @@ function CategoryPage({ addToCart, cartItems}) {
 
 
 
-      <h2 className="text-3xl font-bold ml-12 text-[#0A005A] dark:text-[#FC411E]">
+      <h2 className="text-3xl font-bold ml-4 lg:ml-12 text-[#0A005A] dark:text-[#FC411E]">
         {category}
       </h2>
       <section id="products" className="w-full mt-4">
--- src/components/searchResults.jsx
+++ src/components/searchResults.jsx
@@ -21,7 +21,7 @@ export default function SearchResults({ searchQuery }) {
     <div className="mt-16">
       
       <div>
-        <nav className="flex lg:ml-12" aria-label="Breadcrumb ">
+        <nav className="flex ml-4 lg:ml-12 mb-2" aria-label="Breadcrumb ">
           <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
           <li className="inline-flex items-center">
       <a href="/" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
