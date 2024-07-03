915168b9df28425834d19bb987be4a2bc65162d7
Houstin0 <houstin0000@gmail.com>
2024-07-04 00:32:13 +0300

    section 1 done

--- src/components/ProductPopup.jsx
+++ src/components/ProductPopup.jsx
@@ -97,7 +97,7 @@ function ProductPopup({ product, onClose, addToCart }) {
                 <div className="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
                   <button
                     onClick={() => addToCart(product)}
-                    className="text-white mt-4 sm:mt-0 bg-blue-800 hover:bg-blue-500 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
+                    className="text-white mt-4 sm:mt-0 bg-[#0A005A] hover:bg-[#FC411E] focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
                   >
                     <svg className="w-5 h-5 -ms-2 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
--- src/components/productCards.jsx
+++ src/components/productCards.jsx
@@ -28,25 +28,25 @@ function ProductCards({ products ,addToCart}) {
         <div
         key={product.id}
         className="max-w-sm bg-gray-300 rounded-lg shadow dark:bg-gray-900"
-        onClick={() => openPopup(product)} 
+         
       >
           {/* Product Image */}
-          <a href="#">
+          <div onClick={() => openPopup(product)}>
             <img
-              className="p-4 rounded-t-lg"
+              className="p-4 rounded-t-lg cursor-pointer"
               src={product.image}
               alt="product"
             />
-          </a>
+          </div>
           {/* Product Details */}
           <div className="px-5 pb-5">
-            <a href="#">
+            <div onClick={() => openPopup(product)} className="cursor-pointer ">
               <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                 {product.title}
               </h5>
-            </a>
+            </div>
             {/* Rating */}
-            <div className="flex items-center mt-2.5 mb-5">
+            <div  onClick={() => openPopup(product)} className="flex items-center mt-2.5 mb-5">
               <div className="flex items-center space-x-1 rtl:space-x-reverse">
                 {[...Array(Number(product.rating))].map((_, index) => (
                   <svg
@@ -65,13 +65,22 @@ function ProductCards({ products ,addToCart}) {
                 {product.rating}
               </span>
             </div>
-            {/* Price and Order Button */}
-            <div className="flex items-center justify-between">
+            {/* Price */}
+            <div  onClick={() => openPopup(product)} className="flex items-center justify-between">
               <span className="text-3xl font-bold text-gray-900 dark:text-white">
                 Ksh.{product.price}
               </span>
-       
+           
             </div>
+            <button
+                    onClick={() => addToCart(product)}
+                    className="text-white mt-8 bg-[#0A005A] hover:bg-[#FC411E] focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
+                  >
+                    <svg className="w-5 h-5 -ms-2 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
+                    </svg>
+                    Add to cart
+                  </button>
           </div>
         </div>
       ))}
