23b19981935ea9f5a12b88f4275960cdd9ce846b
Houstin0 <houstin0000@gmail.com>
2024-07-04 00:47:25 +0300

    removed focus on buttons

--- src/components/shoppingCart.jsx
+++ src/components/shoppingCart.jsx
@@ -99,9 +99,10 @@ function ShoppingCart({ addToCart, cartItems, removeFromCart, updateQuantity }) 
             
               
                      <button onClick={() => removeFromCart(item.id)} type="button" className="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500">
-                       <svg className="me-1.5 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"strokeWidth="24" height="24" fill="none" viewBox="0 0 24 24">
-                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18 17.94 6M18 18 6.06 6" />
-                       </svg>
+                       <svg className="me-1.5 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+</svg>
+
                        Remove
                      </button>
                   </div>
