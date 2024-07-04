b7e65aa17e0049eae380b4a80fd1297a5748b5dd
Houstin0 <houstin0000@gmail.com>
2024-07-04 06:36:00 +0300

    removed ratings

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,7 +3,6 @@ import { NavLink, useNavigate, useLocation  } from "react-router-dom";
 import productsData from "../db.json";
 
 export default function Navbar({ onSearch, cartItems }) {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [searchSuggestions, setSearchSuggestions] = useState([]);
   const [isSearchVisible, setIsSearchVisible] = useState(false);
@@ -17,9 +16,7 @@ export default function Navbar({ onSearch, cartItems }) {
 const navigate = useNavigate();
 const location = useLocation();
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+ 
 
 
    
@@ -122,7 +119,7 @@ const location = useLocation();
           Madolla <span className="ml-2 text-[#FC411E] "> vape</span> 
         </NavLink>
 
-        <div className="ml-auto mr-2 flex items-center space-x-2">
+        <div className="ml-auto mr-6 flex items-center space-x-2">
           {!isSearchVisible ? (
             <button
   onClick={toggleSearchVisibility}
@@ -250,88 +247,10 @@ const location = useLocation();
 
 
 
-        <button
-          onClick={toggleMenu}
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700"
-          aria-controls="navbar-default"
-          aria-expanded={isMenuOpen ? "true" : "false"}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
+        
 
         
-        <div
-          className={`${
-            isMenuOpen ? "block" : "hidden"
-          } w-full md:block md:w-auto`}
-          id="navbar-default"
-        >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 mr-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-gray-100 dark:bg-black md:dark:bg-black dark:border-gray-700">
-            <li>
-            {/* <button
-              id="theme-toggle"
-              type="button"
-              onClick={toggleDarkMode}
-              className=" text-gray-500 dark:text-gray-400  lg:hover:bg-gray-200 lg:dark:hover:bg-gray-700  rounded-lg text-sm p-1"
-            >
-              <svg
-                id="theme-toggle-dark-icon"
-                className="hidden w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-              </svg>
-              <svg
-                id="theme-toggle-light-icon"
-                className="hidden w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button> */}
-            </li>
-            {/* <li>
-              <NavLink
-                to="/"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Home
-              </NavLink>
-            </li> */}
-            
-           
 
-
-  
- 
-          </ul>
-
-         
-        </div>
 
 
       </div>
--- src/components/productCards.jsx
+++ src/components/productCards.jsx
@@ -23,7 +23,7 @@ function ProductCards({ products ,addToCart}) {
   }, [selectedProduct]);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center mx-4 lg:mx-12 mb-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 justify-center mx-4 lg:mx-12 mb-6">
       {products.map((product) => (
         <div
         key={product.id}
@@ -31,9 +31,9 @@ function ProductCards({ products ,addToCart}) {
          
       >
           {/* Product Image */}
-          <div onClick={() => openPopup(product)}>
+          <div onClick={() => openPopup(product)} >
             <img
-              className="p-4 rounded-t-lg cursor-pointer"
+              className="p-4 rounded-t-lg cursor-pointer md:h-[360px]"
               src={product.image}
               alt="product"
             />
@@ -44,26 +44,6 @@ function ProductCards({ products ,addToCart}) {
               <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                 {product.title}
               </h5>
-            </div>
-            {/* Rating */}
-            <div  onClick={() => openPopup(product)} className="flex items-center mt-2.5 mb-5">
-              <div className="flex items-center space-x-1 rtl:space-x-reverse">
-                {[...Array(Number(product.rating))].map((_, index) => (
-                  <svg
-                    key={index}
-                    className="w-4 h-4 text-yellow-300"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 22 20"
-                  >
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                  </svg>
-                ))}
-              </div>
-              <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
-                {product.rating}
-              </span>
             </div>
             {/* Price */}
             <div  onClick={() => openPopup(product)} className="flex items-center justify-between">
