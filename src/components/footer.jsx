84bd110ab344e51e241e16944ed60612a93b3580
Houstin0 <houstin0000@gmail.com>
2024-08-06 20:28:17 +0300

    removed mb from footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -15,7 +15,7 @@ export default function Footer() {
   };
 
   return (
-    <footer className="bg-gray-300 rounded-lg shadow dark:bg-gray-900 m-4 mb-4 md:mx-12">
+    <footer className="bg-gray-300 rounded-lg shadow dark:bg-gray-900 mx-4 md:mx-12">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <a
