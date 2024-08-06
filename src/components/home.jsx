01917e0d96f7c86b8170fd0c35c06d60dcee03c1
Houstin0 <houstin0000@gmail.com>
2024-08-06 19:18:06 +0300

    remove product 10

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -27,7 +27,7 @@ function Home({ addToCart, cartItems }) {
   };
 
   useEffect(() => {
-    const interval = setInterval(nextSlide, 3000);
+    const interval = setInterval(nextSlide, 5000);
     return () => clearInterval(interval);
   }, []);
 
